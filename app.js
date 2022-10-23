478752b98ccfbf72c0b2b91dde999960cdb5f2ce
Johnny Crisp <johnnycrisp2@gmail.com>
2022-10-23 16:21:33 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const arrowOne = document.querySelector('.round');
+const arrowTwo = document.querySelector('.round-two');
+const input = document.querySelector('#input-field');
+const backgroundNoise = document.querySelector('#background-noise');
+const album = document.querySelector('#background-noise .card');
+
+arrowOne.addEventListener('click', ()=> {
+    input.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+})
+
+arrowTwo.addEventListener('click', ()=> {
+    backgroundNoise.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+})
+
+
+backgroundNoise.addEventListener('click', (e)=>{
+    e.target.parentNode.classList.add('selected');
+})
+
+// text animation
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "hi.", "want to do a thing?", "hit the arrow"];
+  
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span class="caret" aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback === 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 2000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
